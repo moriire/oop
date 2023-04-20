484aecb255f0fea3faeb85e0dcb6065d4e3713ea
Mobolaji Abdulsalam <ibraheemabdulsalam@gmail.com>
2023-04-20 02:49:19 -0700

    sqlite->mysql

--- backend/ecstasy/settings.py
+++ backend/ecstasy/settings.py
@@ -93,8 +93,11 @@ WSGI_APPLICATION = 'ecstasy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'oop$default',
+        'USER': 'oop',
+        'PASSWORD': 'acsolotltd',
+        'HOST': 'oop.mysql.pythonanywhere-services.com',
     }
     
 }
